f1e244012ee095d3787ad91018436ea8e5f0b09a
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2012-06-28 22:47:17 +0000

    ioi_filtdown_cfg.m & ioi_filtdown_run.m process ROIs/seeds and whole time-series ioi_fc_GLM_on_ROI_cfg.m & ioi_fc_GLM_on_ROI_run.m performs GLM on ROI and/or full image time course. ioi_extract_roi_time_series_cfg.m changed display name Some menu items renamed in ioi11.m & tbx_cfg_ioi11.m

--- spm/ioi11/ioi_fc_GLM_on_ROI_cfg.m
+++ spm/ioi11/ioi_fc_GLM_on_ROI_cfg.m
@@ -29,14 +29,23 @@ regressBrainSignal.values   = {1,0};
 regressBrainSignal.val      = {1};
 regressBrainSignal.help     = {'Include global brain signal as a regressor'};
 
+% Regress signal from whole image time-course
+wholeImage      = cfg_menu;
+wholeImage.tag  = 'wholeImage';
+wholeImage.name = 'Regress signal from whole image time-course';
+wholeImage.labels = {'False','True'};
+wholeImage.values = {0,1};
+wholeImage.val  = {1};
+wholeImage.help = {'Regress signal from whole image time-course. It creates a new sub-folder for each session'};
+
 % Executable Branch
 filtdown1      = cfg_exbranch;       % This is the branch that has information about how to run this module
 filtdown1.name = 'GLM regression';             % The display name
 filtdown1.tag  = 'fc_GLM_on_ROI1'; %Very important: tag is used when calling for execution
-filtdown1.val  = {IOImat redo1 IOImatCopyChoice ROI_choice session_choice IC regressBrainSignal};    % The items that belong to this branch. All items must be filled before this branch can run or produce virtual outputs
+filtdown1.val  = {IOImat redo1 IOImatCopyChoice ROI_choice session_choice IC regressBrainSignal wholeImage};    % The items that belong to this branch. All items must be filled before this branch can run or produce virtual outputs
 filtdown1.prog = @ioi_fc_GLM_on_ROI_run;  % A function handle that will be called with the harvested job to run the computation
 filtdown1.vout = @ioi_cfg_vout_fc_GLM_on_ROI; % A function handle that will be called with the harvested job to determine virtual outputs
-filtdown1.help = {'GLM regression of global brain signal from ROI/seeds time trace in order to remove global source of variance'};
+filtdown1.help = {'GLM regression of global brain signal from ROI/seeds and whole image time trace in order to remove global source of variance'};
 
 return
 
--- spm/ioi11/ioi_filtdown_cfg.m
+++ spm/ioi11/ioi_filtdown_cfg.m
@@ -36,14 +36,23 @@ downFreq.num        = [1 1];                            % Number of inputs requi
 downFreq.val        = {1};                              % Default value
 downFreq.help       = {'Enter downsampling frequency in Hz. A target sampling frequency will be generated, which may however be only approximately equal to the specified downsampling frequency, but it will correspond to the actual frequency of selecting every Nth point'};
 
+% Filter and downsample whole images
+wholeImage      = cfg_menu;
+wholeImage.tag  = 'wholeImage';
+wholeImage.name = 'Filter and downsample whole image time-series';
+wholeImage.labels = {'False','True'};
+wholeImage.values = {0,1};
+wholeImage.val  = {1};
+wholeImage.help = {'Filter and downsample whole image time-series. It creates a new sub-folder for each session'};
+
 % Executable Branch
 filtdown1      = cfg_exbranch;       % This is the branch that has information about how to run this module
-filtdown1.name = 'Temporal filtering and Downsampling of ROIs';             % The display name
+filtdown1.name = 'Temporal filtering and downsampling of seeds & whole image series';             % The display name
 filtdown1.tag  = 'filtdown1'; %Very important: tag is used when calling for execution
-filtdown1.val  = {IOImat redo1 IOImatCopyChoice ROI_choice session_choice IC BPFfreq downFreq};    % The items that belong to this branch. All items must be filled before this branch can run or produce virtual outputs
+filtdown1.val  = {IOImat redo1 IOImatCopyChoice ROI_choice session_choice IC BPFfreq downFreq wholeImage};    % The items that belong to this branch. All items must be filled before this branch can run or produce virtual outputs
 filtdown1.prog = @ioi_filtdown_run;  % A function handle that will be called with the harvested job to run the computation
 filtdown1.vout = @ioi_cfg_vout_filtdown; % A function handle that will be called with the harvested job to determine virtual outputs
-filtdown1.help = {'Temporal band-pass filtering and downsampling of a given time trace [HbO/HbR/Flow] to be used as a contrast.'};
+filtdown1.help = {'Temporal band-pass filtering and downsampling of a given time trace [HbO/HbR/Flow], either on a seed or on the whole image series.'};
 
 return
 
