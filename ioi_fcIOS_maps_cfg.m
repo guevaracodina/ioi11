e60f12eab04967e955bd87a2392e12353d382cce
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2012-12-14 00:12:20 +0000

    //EGC ioi_fcIOS_maps_run.m, ioi_fcIOS_maps_cfg.m - Plots multiple correlation maps at the specified size and resolution. A semi-transparent functional correclation map is overlaid on the anatomical map. Images are saved as PNG. Pending to do: Choose colormap, background color, filetype from the GUI

--- ioi_fcIOS_maps_cfg.m
+++ ioi_fcIOS_maps_cfg.m
@@ -9,6 +9,8 @@ function fcIOS_maps         = ioi_fcIOS_maps_cfg
 IOImat                      = ioi_dfg_IOImat(2);
 % IOI copy/overwrite method
 IOImatCopyChoice            = ioi_dfg_IOImatCopyChoice('fcIOS_maps');
+% Choose ROI selection method (all/selected)
+ROI_choice                  = ioi_dfg_ROI_choice;
 % Colors to include (OD,HbO,HbR,HbT,Flow,CMRO2)
 IC                          = ioi_dfg_include_colors(0,1,1,1,1);
 % Choose session selection method (all/selected)
@@ -22,11 +24,47 @@ parent_results_dir.filter   = 'dir';
 parent_results_dir.num      = [1 1];
 parent_results_dir.help     = {'Select the directory where figures will be saved.'}';
 
+% Figure size
+figSize                     = cfg_entry;
+figSize.tag                 = 'figSize';
+figSize.name                = 'Figure size';
+figSize.strtype             = 'r';
+figSize.num                 = [1 4];
+figSize.val{1}              = [1 1 2 2];
+figSize.help                = {'Enter figure size in inches'};
+
+% Figure resolution
+figRes                      = cfg_entry;
+figRes.tag                  = 'figRes';
+figRes.name                 = 'Figure resolution';
+figRes.strtype              = 'r';
+figRes.num                  = [1 1];
+figRes.val{1}               = 1200;
+figRes.help                 = {'Enter figure resolution in dpi [300-1200]'};
+
+% Colormap range
+figRange                    = cfg_entry;
+figRange.tag                = 'figRange';
+figRange.name               = 'Colormap range';
+figRange.strtype            = 'r';
+figRange.num                = [1 2];
+figRange.val{1}             = [-1 1];
+figRange.help               = {'Enter colormap range'};
+
+% Alpha transparency values
+figAlpha                    = cfg_entry;
+figAlpha.tag                = 'figAlpha';
+figAlpha.name               = 'Transparency';
+figAlpha.strtype            = 'r';
+figAlpha.num                = [1 1];
+figAlpha.val{1}             = 0.8;
+figAlpha.help               = {'Enter colormap transparency, between 0 and 1'};
+
 % Executable Branch
 fcIOS_maps                  = cfg_exbranch; % This is the branch that has information about how to run this module
 fcIOS_maps.name             = 'fcIOS maps'; % The display name
 fcIOS_maps.tag              = 'fcIOS_maps'; %Very important: tag is used when calling for execution
-fcIOS_maps.val              = {IOImat IOImatCopyChoice IC session_choice parent_results_dir };    % The items that belong to this branch. All items must be filled before this branch can run or produce virtual outputs
+fcIOS_maps.val              = {IOImat IOImatCopyChoice ROI_choice IC session_choice parent_results_dir figSize figRes figRange figAlpha};    % The items that belong to this branch. All items must be filled before this branch can run or produce virtual outputs
 fcIOS_maps.prog             = @ioi_fcIOS_maps_run; % A function handle that will be called with the harvested job to run the computation
 fcIOS_maps.vout             = @ioi_cfg_vout_fcIOS_maps; % A function handle that will be called with the harvested job to determine virtual outputs
 fcIOS_maps.help             = {'Plots multiple correlation maps at the same scale'};
--- ioi_fcIOS_maps_run.m
+++ ioi_fcIOS_maps_run.m
@@ -15,6 +15,8 @@ function out = ioi_fcIOS_maps_run(job)
 
 % Select a subset of sessions
 [all_sessions selected_sessions] = ioi_get_sessions(job);
+% Colors to include
+IC = job.IC;
 
 % Loop over subjects
 for SubjIdx = 1:length(job.IOImat)
@@ -26,7 +28,15 @@ for SubjIdx = 1:length(job.IOImat)
         brainMaskVol = spm_vol(IOI.fcIOS.mask.fname);
         brainMask = logical(spm_read_vols(brainMaskVol));
         % Read anatomical NIFTI file
-        im_anat = ioi_read_vol(IOI.res.file_anat);
+        imAnatVol = spm_vol(IOI.res.file_anat);
+        imAnat = spm_read_vols(imAnatVol);
+        
+        if IOI.res.shrinkageOn
+            vx = [IOI.res.shrink_x IOI.res.shrink_y 1];
+        else
+            vx = [1 1 1];
+        end
+        
         % Loop over sessions
         for s1 = 1:length(IOI.sess_res)
             if all_sessions || sum(s1==selected_sessions)
@@ -34,14 +44,42 @@ for SubjIdx = 1:length(job.IOImat)
                 for c1=1:length(IOI.sess_res{s1}.fname)
                     doColor = ioi_doColor(IOI,c1,IC);
                     if doColor
-                        % Load correlation data
-                        if size(brainMask,1)~= size(y,1)|| size(brainMask,2)~= size(y,2)
-                            brainMask = ioi_MYimresize(brainMask, [size(y,1) size(y,2)]);
-                        end
-                        % Mask data
-                        
-                        % display overlay data
-                       
+                        [all_ROIs selected_ROIs] = ioi_get_ROIs(job);
+                        nROI = 1:length(IOI.res.ROI); % All the ROIs
+                        % Loop over ROI/seeds
+                        for r1 = nROI,
+                            if (all_ROIs || sum(r1==selected_ROIs)) && IOI.fcIOS.corr.corrMapOK{r1}{s1, c1}
+                                %% Load correlation data
+                                tempCorrMapVol = spm_vol(IOI.fcIOS.corr.corrMapName{r1}{s1, c1});
+                                tempCorrMap = spm_read_vols(tempCorrMapVol);
+                                [~, oldName, oldExt] = fileparts(IOI.fcIOS.SPM.fnameROInifti{r1}{s1, c1});
+                                newName = [sprintf('%s_R%02d_S%02d_C%d',IOI.subj_name,r1,s1,c1) '_fcIOS_corrMapMasked'];
+                                dir_corrMapMaskedfig = fullfile(dir_ioimat,'fig_corrMapMasked');
+                                if ~exist(dir_corrMapMaskedfig,'dir'), mkdir(dir_corrMapMaskedfig); end
+                                
+                                %% Mask out non-brain voxels
+                                if size(brainMask,1)~= size(tempCorrMap,1)|| size(brainMask,2)~= size(tempCorrMap,2)
+                                    brainMask = ioi_MYimresize(brainMask, [size(tempCorrMap,1) size(tempCorrMap,2)]);
+                                end
+                                % Make NaN all the transparent pixels
+                                tempCorrMap(~brainMask) = NaN;
+                                y = tempCorrMap;
+                                IOI.fcIOS.corr.corrMapNameMask{r1}{s1, c1} = fullfile(dir_corrMapMaskedfig,[newName oldExt]);
+                                % Save NIfTI to use slover
+                                ioi_save_nifti(y, IOI.fcIOS.corr.corrMapNameMask{r1}{s1, c1}, vx);
+                                
+                                %% display overlay data
+                                % Get parameters for overlay
+                                anatomical      = IOI.res.file_anat;
+                                corrMap         = IOI.fcIOS.corr.corrMapNameMask{r1}{s1, c1};
+                                colorNames      = fieldnames(IOI.color);
+                                % h = ioi_overlay_map(anatomical, positiveMap, negativeMap, mapRange, titleString)
+                                internal_overlay_map(anatomical, corrMap,  job, newName);
+                                
+                                %% Print figure
+                                
+                            end
+                        end % ROIs loop
                     end
                 end % colors loop
             end
@@ -56,5 +94,109 @@ for SubjIdx = 1:length(job.IOImat)
     end
 end % loop over subjects
 end % Main function
+
+function [h, varargout] = internal_overlay_map(anatomical, positiveMap,  job, titleString)
+% Creates an overlay image composed of positive & negative functional maps (any
+% contrast, correlation, z, p-value, etc.) onto an anatomical image.
+% The images must be saved as NIfTI (.nii) files and they can have different
+% sizes. This function makes use of slover and paint functions of SPM8.
+% The positive map is plotted in hot colormap, the negative map is cold and the
+% anatomical is grayscale by default. The image has the following orientation:
+% 
+%         |
+%         |
+%         | Left
+%         |        
+% ________|
+% Rostral
+% 
+% SYNTAX
+% h = ioi_overlay_map(anatomical,positiveMap,negativeMap,mapRange,titleString)
+% INPUTS
+% anatomical    NIfTI (.nii) filename with anatomical image for background.
+% positiveMap   NIfTI (.nii) filename with positive functional map on the
+%               foreground.
+% negativeMap   NIfTI (.nii) filename with negativ functional map on the
+%               foreground.
+% titleString   String with the title to be displayed.
+% mapRange      2-element cell with the data range (positive&negative) to be displayed.
+% OUTPUT
+% h             Handle to the figure
+%_______________________________________________________________________________
+% Copyright (C) 2012 LIOM Laboratoire d'Imagerie Optique et Moléculaire
+%                    École Polytechnique de Montréal
+%_______________________________________________________________________________
+
+
+% Necessary to get rid of old overlay objects
+clear imagesOverlay
+% Make cell with file names
+imagesOverlay{1,1}  = anatomical;           % Anatomical image
+imagesOverlay{2,1}  = positiveMap;          % Pos. correlation
+% imagesOverlay{3,1}  = negativeMap;          % Neg. correlation
+
+% Handle of current figure;
+h = figure;
+set(h,'color','k')
+
+% Get range
+mapRange        = {job.figRange};
+
+% Make mapRange a column vector
+if size(mapRange, 1) == 1
+    mapRange = mapRange';
+    mapRange = cellfun(@(x) x', mapRange, 'UniformOutput', false);
+end
+
+% Get 
+
+% Create overlay object
+slObj = slover(char(imagesOverlay));
+
+slObj.slices = 1;                           % For IOI images only 1 slice (2D)
+                                            % Automatic range for image 1 (anatomy)
+slObj.img(2).range = mapRange{1};           % Range for positive map
+
+slObj.img(1).type = 'truecolour';           % Anatomical image
+slObj.img(2).type = 'truecolour';           % Functional map
+
+slObj.img(1).cmap = gray(256);              % Colormap for anatomy
+slObj.img(2).cmap = jet(256);               % Colormap for positive map
+
+% slObj.cbar = [2 3];                         % Plot colorbars for images 2 & 3
+slObj.area.valign = 'middle';               % Vertical alignment
+slObj.area.halign = 'center';               % Horizontal alignment
+
+slObj.img(1).prop = 1;                      % Proportion of intensity for anatomy
+slObj.img(2).prop = job.figAlpha;           % Proportion of intensity for positive map
+
+slObj.img(1).outofrange =  {0 255};         % Behavior for image values out of range 
+slObj.img(2).outofrange =  {0 255};
+
+slObj.labels = 'none';                      % No labels on this slice
+  
+% Redraw the object (e.g. after window maximization)
+slObj = paint(slObj);
+
+% Change figure name
+set(slObj.figure,'Name',titleString);
+
+varargout{1} = slObj;
+
+% Specify window units
+set(h, 'units', 'inches')
+% Change figure and paper size
+set(h, 'Position', job.figSize)
+set(h, 'PaperPosition', job.figSize)
+% Refresh figure
+slObj = paint(slObj);
+% Save as PNG at the user-defined resolution
+print(h, '-dpng', ...
+    fullfile(job.parent_results_dir{1}, titleString),...
+    sprintf('-r%d',job.figRes));
+% Return the property to its default
+set(h, 'units', 'pixels')
+close(h)
+end
 
 % EOF
