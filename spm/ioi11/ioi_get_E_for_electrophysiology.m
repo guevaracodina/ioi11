060f7ba7fcd49546f621d39b6733de6eb90f8574
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2012-12-21 20:39:56 +0000

    seizure onsets are created manually; F-test is added in GLM

--- spm/ioi11/ioi_get_E_for_electrophysiology.m
+++ spm/ioi11/ioi_get_E_for_electrophysiology.m
@@ -4,40 +4,63 @@ stim_choice=0;
 if isfield(job.stim_choice,'electro_stims')
     %choose first or second electrode
     if isfield(job.stim_choice.electro_stims,'seizure_detection')
-        E.szOn = 1; E.spkOn = 0;
-        electro_stims = job.stim_choice.electro_stims.seizure_detection;
+        if isfield(job.stim_choice.electro_stims.seizure_detection,'seizure_detection_automatically')
+        E.szOn_auto = 1; E.szOn_manual = 0; E.spkOn = 0;
+        electro_stims = job.stim_choice.electro_stims.seizure_detection.seizure_detection_automatically;
         E.tb = electro_stims.seizure_tb;
         E.ta = electro_stims.seizure_ta;
         %minimal distance between peaks in seconds
         E.dP = electro_stims.seizure_dP;
-        E.electrophysiology_onset_name = electro_stims.seizure_onset_name;  
+        E.electrophysiology_onset_name = electro_stims.seizure_onset_name;
+        %number of standard deviations
+        E.nSD = electro_stims.nSD;
+        %minimum standard deviation imposed
+        E.mbSD = electro_stims.mbSD;
+        else
+            E.szOn_auto = 0; E.szOn_manual = 1; E.spkOn = 0;
+            electro_stims = job.stim_choice.electro_stims.seizure_detection.seizure_detection_manually;
+            E.onset = electro_stims.seizure_onset;  
+            E.offset = electro_stims.seizure_offset;
+            E.tb = electro_stims.seizure_tb;  
+            E.ta = electro_stims.seizure_ta;
+            E.electrophysiology_onset_name = electro_stims.seizure_onset_name;   
+            E.nSD = electro_stims.nSD;
+        end
     end
     if isfield(job.stim_choice.electro_stims,'spike_detection')
-        E.szOn = 0; E.spkOn = 1;
+        E.szOn_auto = 0; E.szOn_manual = 0; E.spkOn = 1;
         electro_stims = job.stim_choice.electro_stims.spike_detection;
         E.tb = electro_stims.spike_tb;
         E.ta = electro_stims.spike_ta;
         %minimal distance between peaks in seconds
         E.dP = electro_stims.spike_dP/1000;
-        E.electrophysiology_onset_name = electro_stims.spike_onset_name;  
+        E.electrophysiology_onset_name = electro_stims.spike_onset_name; 
+        %number of standard deviations
+        E.nSD = electro_stims.nSD;
+        %minimum standard deviation imposed
+        E.mbSD = electro_stims.mbSD;
     end
     if isfield(job.stim_choice.electro_stims,'spontaneous_activity_detection')
-        E.szOn = 0; E.spkOn = 1;
+        E.szOn_auto = 0; E.szOn_manual = 0; E.spkOn = 1;
         electro_stims = job.stim_choice.electro_stims.spontaneous_activity_detection;
         E.tb = electro_stims.spike_tb;
         E.ta = electro_stims.spike_ta;
         %minimal distance between peaks in seconds
         E.dP = electro_stims.spike_dP/1000;
-        E.electrophysiology_onset_name = electro_stims.spike_onset_name;      
+        E.electrophysiology_onset_name = electro_stims.spike_onset_name;
+        %number of standard deviations
+        E.nSD = electro_stims.nSD;
+        %minimum standard deviation imposed
+        E.mbSD = electro_stims.mbSD;
     end
     E.el_choice = electro_stims.electrophysiology_choice;
     stim_choice = 1;
     %sampling frequency
     E.sf = electro_stims.sf;
     %number of standard deviations
-    E.nSD = electro_stims.nSD;
-    %minimum standard deviation imposed
-    E.mbSD = electro_stims.mbSD;    
+%     E.nSD = electro_stims.nSD;
+%     %minimum standard deviation imposed
+%     E.mbSD = electro_stims.mbSD;    
     %HPF
     if isfield(electro_stims.hpf_butter,'hpf_butter_On')
         E.electro_hpf_butter_On = 1;
