f3329394ec78fb5609a845b3813baa8b34611a8e
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-08-05 17:26:00 +0000

    // EGC ioi_brainmask_cfg.m, ioi_brainmask_run.m - Added the possibility to Ask for a previous brain mask. Also the creation of new folder for IOI matrix. ioi_cmro2_run.m - Fixed error in resizing brain mask ioi_create_roi_cfg.m - Minor fix: changed long name in options to something shorter. Improves display of batch window ioi_roi_spline.m - Added comment

--- ioi_brainmask_cfg.m
+++ ioi_brainmask_cfg.m
@@ -7,27 +7,40 @@ function mask1 = ioi_brainmask_cfg
 %_______________________________________________________________________________
 
 % Select IOI.mat
-IOImat          = ioi_dfg_IOImat(1);
+IOImat                      = ioi_dfg_IOImat(1);
 % Force processing
-redo1           = ioi_dfg_redo(0);
+redo1                       = ioi_dfg_redo(0);
+% IOI copy/overwrite method
+IOImatCopyChoice            = ioi_dfg_IOImatCopyChoice('brainMask');
+
+% Choose whether interface should ask whether to use a previously saved list of ROI
+SelectPreviousMask          = cfg_menu;
+SelectPreviousMask.tag      = 'SelectPreviousMask';
+SelectPreviousMask.name     = 'Ask for a possible previous brain mask';
+SelectPreviousMask.labels   = {'No','Yes'};
+SelectPreviousMask.values   = {false,  true};
+SelectPreviousMask.val      = {false};
+SelectPreviousMask.help     = {'If this option is selected, then before selection of brain mask,'
+    'The interface will ask the user if they want to use a brain mask from elsewhere.'
+    'This brain mask can then be selected by locating the IOI.mat structure that contains the information.'
+    'Useful when re-running the pipeline'}';
 
 % Executable Branch
-mask1           = cfg_exbranch; % This is the branch that has information about how to run this module
-mask1.name      = 'Create Brain Mask'; % The display name
-mask1.tag       = 'mask1'; %Very important: tag is used when calling for execution
-mask1.val       = {IOImat redo1};    % The items that belong to this branch. All items must be filled before this branch can run or produce virtual outputs
-mask1.prog      = @ioi_brainmask_run;  % A function handle that will be called with the harvested job to run the computation
-mask1.vout      = @ioi_cfg_vout_brainmask; % A function handle that will be called with the harvested job to determine virtual outputs
-mask1.help      = {'Manual segmentation of the brain to provide a mask for fcOIS analysis'};
+mask1                       = cfg_exbranch; % This is the branch that has information about how to run this module
+mask1.name                  = 'Create Brain Mask'; % The display name
+mask1.tag                   = 'mask1'; %Very important: tag is used when calling for execution
+mask1.val                   = {IOImat redo1 IOImatCopyChoice SelectPreviousMask};    % The items that belong to this branch. All items must be filled before this branch can run or produce virtual outputs
+mask1.prog                  = @ioi_brainmask_run;  % A function handle that will be called with the harvested job to run the computation
+mask1.vout                  = @ioi_cfg_vout_brainmask; % A function handle that will be called with the harvested job to determine virtual outputs
+mask1.help                  = {'Manual segmentation of the brain to provide a mask for fcOIS analysis'};
 
 return
 
 % make IOI.mat available as a dependency
-function vout = ioi_cfg_vout_brainmask(job)
-vout            = cfg_dep; % The dependency object
-vout.sname      = 'IOI.mat'; % Displayed dependency name
-vout.src_output = substruct('.','IOImat'); %{1}); %,'IOImat');
-%substruct('()',{1}); % The output subscript reference. This could be any reference into the output variable created during computation
-vout.tgt_spec   = cfg_findspec({{'filter','mat','strtype','e'}});
+function vout               = ioi_cfg_vout_brainmask(job)
+vout                        = cfg_dep; % The dependency object
+vout.sname                  = 'IOI.mat'; % Displayed dependency name
+vout.src_output             = substruct('.','IOImat'); %{1}); %,'IOImat');
+vout.tgt_spec               = cfg_findspec({{'filter','mat','strtype','e'}});
 
 % EOF
--- ioi_brainmask_run.m
+++ ioi_brainmask_run.m
@@ -5,7 +5,7 @@ function [out, BW_mask] = ioi_brainmask_run(job)
 % SYNTAX:
 % out = ioi_brainmask_run(job)
 % INPUTS:
-% job
+% job       Matlab batch job structure
 % OUTPUTS:
 % out       Structure containing the names of IOI matrices
 % BW_mask   Binary mask that contains brain pixels
@@ -14,13 +14,22 @@ function [out, BW_mask] = ioi_brainmask_run(job)
 %                    École Polytechnique de Montréal
 %_______________________________________________________________________________
 
-for SubjIdx=1:length(job.IOImat)
+try
+    SelectPreviousMask = job.SelectPreviousMask;
+catch
+    SelectPreviousMask = false;
+end
+
+% Loop over subjects
+for SubjIdx = 1:length(job.IOImat)
     try
         tic
         clear IOI
         % Load IOI.mat information
-        IOImat = job.IOImat{SubjIdx};
-        load(IOImat);
+        [IOI IOImat dir_ioimat] = ioi_get_IOI(job,SubjIdx);
+        % Load IOI.mat information
+        % IOImat = job.IOImat{SubjIdx};
+        % load(IOImat);
         
         if ~isfield(IOI, 'fcIOS')
             % Create fcIOS field to contain the whole structure of fcIOS
@@ -33,51 +42,80 @@ for SubjIdx=1:length(job.IOImat)
             IOI.fcIOS(1).corr = struct([]);
         end
         if ~isfield(IOI.fcIOS.mask,'maskOK') || job.force_redo
-            % 1-Lis une image nifti de type anatomique (dans notre cas c'est l'image
-            % verte de l'IOI) qui n'est qu'une tranche (sans temps)
-            [dirName fileName fileExt] = fileparts(IOI.res.file_anat);
-            fileName = strcat(fileName, fileExt);
             
-            % Read anatomical NIFTI file
-            im_anat = ioi_read_vol(fullfile(dirName, fileName));
-            
-            % 2-Afficher et utiliser imroi pour que l'usager puisse manuellement
-            % identifier la zone d'intérêt contenant le cerveau (on click un
-            % polygone)
-            
-            % Display anatomical image on SPM graphics window
-            spm_figure('GetWin', 'Graphics');
-            spm_figure('Clear', 'Graphics');
-            
-            % Start interactive ROI tool to choose spline brain mask
-            % ------------------------------------------------------------------
-            BW_mask = ioi_roi_spline(im_anat);
-            % ------------------------------------------------------------------
-            
-            % Display masked image on SPM graphics window
-            minVal = min(im_anat(:));
-            maxVal = max(im_anat(:));
-            imagesc(im_anat .* BW_mask, [minVal maxVal]);
-            axis image
-            title(['Mask for subject ' IOI.subj_name],'FontSize',13)
-            
-            % 3-Sauver en sortie une image nifti qui s'appelle brainmask.nii qui
-            % vaut 1 dans le cerveau et 0 en dehors.
-            
-            % Create filename according the existing nomenclature at subject level
-            brainMaskName = [IOI.subj_name '_anat_brainmask.nii'];
-            
-            % Create and write a NIFTI file in the subject folder
-            hdr = spm_vol(fullfile(dirName,fileName));
-            ioi_create_vol(fullfile(dirName, brainMaskName), ...
-                hdr.dim, hdr.dt, hdr.pinfo, hdr.mat, hdr.n, BW_mask);
-            if isempty(IOI.fcIOS.mask)
-                % To avoid emptyDotAssignment we create a field
-                IOI.fcIOS.mask = struct('fname', []);
-            end
-            
-            % Identifier dans IOI le nom du fichier masque
-            IOI.fcIOS.mask.fname = fullfile(dirName, brainMaskName);
+            if SelectPreviousMask
+                % Choose a previous brain mask.
+                % Goto interactive window
+                h2 = spm_figure('GetWin', 'Interactive');
+                spm_input(['Subject ' int2str(SubjIdx) 'of ' int2str(length(job.IOImat)) ' (' IOI.subj_name ')' ],'-1','d');
+                SelPrevMask = spm_input('Select a previous brain mask?','+1','y/n');
+                if SelPrevMask == 'y'
+                    [tPrevMask stsPrevMask] = spm_select(1,'mat','Select IOI.mat structure containing information on desired brain mask','',dir_ioimat,'IOI.mat',1);
+                    if stsPrevMask
+                        IOI0 = IOI; %Store current IOI
+                        try
+                            load(tPrevMask);
+                            IOI_withMask = IOI;
+                            IOI = IOI0;
+                            try
+                                IOI.fcIOS.mask(1).fname = IOI_withMask.fcIOS.mask.fname;
+                                clear IOI_withMask
+                            catch
+                                IOI = IOI0;
+                                disp('Specified IOI.mat structure does not contain valid brain mask information')
+                            end
+                        catch
+                            disp('Could not load IOI.mat structure containing desired brain mask')
+                        end
+                    end
+                end
+            else % Choose manually a new brain mask. 
+                % 1-Lis une image nifti de type anatomique (dans notre cas c'est l'image
+                % verte de l'IOI) qui n'est qu'une tranche (sans temps)
+                [dirName fileName fileExt] = fileparts(IOI.res.file_anat);
+                fileName = strcat(fileName, fileExt);
+                
+                % Read anatomical NIFTI file
+                im_anat = ioi_read_vol(fullfile(dirName, fileName));
+                
+                % 2-Afficher et utiliser imroi pour que l'usager puisse manuellement
+                % identifier la zone d'intérêt contenant le cerveau (on click un
+                % polygone)
+                
+                % Display anatomical image on SPM graphics window
+                spm_figure('GetWin', 'Graphics');
+                spm_figure('Clear', 'Graphics');
+                
+                % Start interactive ROI tool to choose spline brain mask
+                % ------------------------------------------------------------------
+                BW_mask = ioi_roi_spline(im_anat);
+                % ------------------------------------------------------------------
+                
+                % Display masked image on SPM graphics window
+                minVal = min(im_anat(:));
+                maxVal = max(im_anat(:));
+                imagesc(im_anat .* BW_mask, [minVal maxVal]);
+                axis image
+                title(sprintf('Mask for subject %s; %d of %d', IOI.subj_name, SubjIdx, length(job.IOImat)),'FontSize',13)
+                
+                % 3-Sauver en sortie une image nifti qui s'appelle brainmask.nii qui
+                % vaut 1 dans le cerveau et 0 en dehors.
+                
+                % Create filename according the existing nomenclature at subject level
+                brainMaskName = [IOI.subj_name '_anat_brainmask.nii'];
+                
+                % Create and write a NIFTI file in the subject folder
+                hdr = spm_vol(fullfile(dirName,fileName));
+                ioi_create_vol(fullfile(dirName, brainMaskName), ...
+                    hdr.dim, hdr.dt, hdr.pinfo, hdr.mat, hdr.n, BW_mask);
+                if isempty(IOI.fcIOS.mask)
+                    % To avoid emptyDotAssignment we create a field
+                    IOI.fcIOS.mask = struct('fname', []);
+                end
+                
+                % Identifier dans IOI le nom du fichier masque
+                IOI.fcIOS.mask.fname = fullfile(dirName, brainMaskName);
+            end % SelectPreviousMask
             % Mask created succesfully!
             IOI.fcIOS.mask.maskOK = true;
             save(IOImat,'IOI');
@@ -90,7 +128,7 @@ for SubjIdx=1:length(job.IOImat)
         disp(exception.stack(1))
         out.IOImat{SubjIdx} = job.IOImat{SubjIdx};
     end % End of try
-end % End of main for
+end % End of main for (loop over subjects)
 end % End of function
 
 %_______________________________________________________________________________
@@ -112,3 +150,5 @@ function im_anat = ioi_read_vol(nifti_anat_file)
 vol = spm_vol(nifti_anat_file);
 im_anat = spm_read_vols(vol);
 end % End ioi_read_vol
+
+% EOF
