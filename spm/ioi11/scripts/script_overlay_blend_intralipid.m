5363203e52634a33aeaba8ddb70ba92c37f1bf5b
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-08-15 00:43:09 +0000

    // EGC ioi_overlay_blend_intralipid.m, script_overlay_blend_intralipid.m - Process images from intralipid script_overlay_blend.m - Minor changes, adapted to new results folder

--- spm/ioi11/scripts/script_overlay_blend_intralipid.m
+++ spm/ioi11/scripts/script_overlay_blend_intralipid.m
@@ -1,21 +1,18 @@
 %% script_overlay_blend
 % String to identify the group
-groupID = 'NC';
+groupID = 'WL';
 % Folder where to save the images
-% figFolder = 'D:\Edgar\Documents\Dropbox\Docs\Carotid\Figures\aligned\';
-figFolder = 'F:\Edgar\Data\IOS_Carotid_Res2\alignment\figures\aligned_CBF\';
+figFolder = 'E:\Edgar\Data\IOS_Resolution\Results\averaged_maps\CBF\R04\';
 % Range of values to map to the full range of colormap: [minVal maxVal]
-fcMapRange = [-0.2733 0.9052];
+fcMapRange = [-1 1];
 % Range of values to map to display non-transparent pixels: [minVal maxVal]
 alphaRange = [-1 1];
 
 %% Code
 dirListNIfTI = dir(fullfile(figFolder, [groupID '*.nii']));
 % Load IOI matrix of the source image
-if strcmp(groupID, 'CC')
-    IOImat = 'D:\Edgar\Data\IOS_Carotid_Res\12_10_19,CC10\GLMfcIOS\corrMap\IOI.mat';
-elseif strcmp(groupID, 'NC')
-    IOImat = 'D:\Edgar\Data\IOS_Carotid_Res\12_10_18,NC09\GLMfcIOS\corrMap\IOI.mat';
+if strcmp(groupID, 'WL')
+    IOImat = 'E:\Edgar\Data\IOS_Resolution\Results\13_07_30,WL01\IOI.mat';
 else
     fprintf('No IOI matrix found for %s.\n', groupID);
     return
@@ -38,7 +35,7 @@ translate = @(a,b) [1 0 a 0; 0 1 b 0; 0 0 1 0; 0 0 0 1];
 % ------------------------------------------------------------------------------
 job(1).figCmap                                  = jet(256);     % colormap
 job(1).figIntensity                             = 1;            % [0 - 1]
-job(1).transM                                   = rotz(pi);     % affine transform
+job(1).transM                                   = rotz(pi/2)*roty(pi/2);     % affine transform
 job(1).figSize                                  = [1.5 1.5];    % inches
 job(1).figRes                                   = 300;          % in dpi
 job(1).drawCircle(1).drawCircle_On(1).circleLW  = 0.8;          % line width
@@ -48,10 +45,19 @@ job.parent_results_dir{1}                       = fullfile(figFolder,'overlay');
 job.generate_figures                            = true;         % display figure
 job.save_figures                                = true;        % save figure
 % ------------------------------------------------------------------------------
-
+nColorLevels = 256;
+r1 = 4;
 % Main loop
 for iFiles = 1:numel(images2overlay)
-    [displayed_pixels{iFiles}, total_pixels(iFiles)] = ioi_overlay_blend(IOImat, job, images2overlay{iFiles}, fcMapRange, alphaRange);
+    [displayed_pixels{iFiles}, total_pixels(iFiles)] = ioi_overlay_blend_intralipid(...
+        IOImat, job, images2overlay{iFiles}, fcMapRange, alphaRange, nColorLevels, r1);
 end
 
+%% Average images
+% for i=1:10
+%     vol(i) = spm_vol(fullfile('E:\Edgar\Data\IOS_Resolution\Results\averaged_maps\CBF\R04',['13_07_30,WL' sprintf('%02d',i) '_R04_S01_C7_fcIOS_map.nii']));
+%     I(:,:,i) = spm_read_vols(vol(i));
+% end
+% avg_I = mean(I,3);
+% ioi_save_nifti(avg_I, 'E:\Edgar\Data\IOS_Resolution\Results\averaged_maps\CBF\R04\AVG_R04_C7.nii', [1 1 1]);
 % EOF
