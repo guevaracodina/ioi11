1e6818e64e7689d267b8005f8ed11736c18bb989
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2015-07-23 20:58:49 +0000

    // EGC ioi_export_2analyzer.m - Exports IOI data as .nir files to be imported in analyzer2 ioi_boxy_write_header.m - Creates a .vhdr file hen exporting data to analyzer2. Modified from nirs_boxy_write_header.m, this version does not need a marker file script_export_2analyzer.m - Makes use of ioi_export_2analyzer.m to batch-process IOI files

--- scripts/script_export_2analyzer.m
+++ scripts/script_export_2analyzer.m
@@ -1,169 +1,21 @@
 %% script_export_2analyzer
-%% Loading data after filtering and downsampling
-% Run spm8 shortcut first
-clc
+%% Adding nirs10 toolbox to the path
+% Run spm8 shortcut first!
+% Run SPM 8
+% cd c:\spm8
+% addpath(genpath('c:\spm8'))
+% spm fmri
+clear; close all; clc
 addpath(genpath('C:\spm8\toolbox\nirs10')); % After running spm8 shortcut
-pathNameParent = 'C:\Edgar\Data\IOIResults\j03\ROItest';
-IOImat = fullfile(pathNameParent, 'IOI.mat');
-load(IOImat)
-if IOI.fcIOS.filtNdown.filtNdownOK
-	% Load filtered & downsampled image time-course
-    vol = spm_vol(IOI.fcIOS.filtNdown.fnameWholeImage{1});
-    y = spm_read_vols(vol);
-end
-% Get image dimensions
-[nX, nY, nZ, nT] = size(y);
-% Reshape as 2-D matrix
-y = reshape(y,[nX*nY nT]);
-% Intrinsic Imaging frequency (after downsampling to 1 Hz)
-Fs = IOI.fcIOS.filtNdown.fs;
-Ts = 1/Fs;
 
-%% Load EKG & Respiration Data
-%  pathNamePhysio = 'C:\Edgar\Data\IOIData20141016\Physio_Monitoring\150642_J03';
-% pathNamePhysio = 'C:\Edgar\Data\IOIData20141023\Physio Monitoring\111141_K04';
-pathNamePhysio = 'C:\Edgar\Data\IOIData20141211\Physio Monitoring\121423';
-% cd (pathNamePhysio)
-physio = ioi_Bin2Mat(pathNamePhysio);
-% fid=fopen(fullfile(pathNamePhysio,'ECG1.bin'),'r');
-% data = uint8(fread(fid));
-% ECG1 = double(swapbytes(typecast(data,'int32')))/8388608*2400;
-% fclose(fid);
-% fid=fopen(fullfile(pathNamePhysio,'ECG2.bin'),'r');
-% data = uint8(fread(fid));
-% ECG2 = double(swapbytes(typecast(data,'int32')))/8388608*2400;
-% fclose(fid);
-% fid=fopen(fullfile(pathNamePhysio,'ECG3.bin'),'r');
-% data = uint8(fread(fid));
-% ECG3 = double(swapbytes(typecast(data,'int32')))/8388608*2400;
-% fclose(fid);
-% fid=fopen(fullfile(pathNamePhysio,'ECG4.bin'),'r');
-% data = uint8(fread(fid));
-% ECG4 = double(swapbytes(typecast(data,'int32')))/8388608*2400;
-% fclose(fid);
-% 
-% %Read resp data
-% fid=fopen(fullfile(pathNamePhysio,'resp.bin'),'r');
-% data = uint8(fread(fid));
-% Resp = double(swapbytes(typecast(data,'int16')));
-% fclose(fid);
+%% Parent folders with IOImat
+pathNameParentList{2} = 'C:\Edgar\Data\IOIResults\j03\ROItest';
 
-ECG_freq = 1000;        % sampling rate (EKG)
-ECG_Ts = 1/ECG_freq;    % sampling time (EKG) interval
-resp_freq = 250;        % sampling rate (respiration)
-resp_Ts = 1/resp_freq;  % sampling time (respiration) interval
-t = 0:resp_Ts:(numel(physio.Resp)-1)*resp_Ts;
 
-%% ECG filtering
-% % 10-200 Hz, Butterworth three-order filter
-% fType = 'butter';
-% BPFfreq = [2 100];
-% filterOrder = 4;
-% % Band-pass filter configuration
-% [z, p, k] = temporalBPFconfig(fType, ECG_freq, BPFfreq, filterOrder);
-% ECGfilt = temporalBPFrun(ECG2, z, p, k);
+%% Folders with physiological data (in the same order as the IOImat list)
+pathNamePhysioList{2} = 'C:\Edgar\Data\IOIData20141211\Physio Monitoring\121423';
 
-%%  Optimal Minimum Order Designs (BPF from 0.01 to 30 Hz)
-% Fp1 — frequency at the edge of the start of the pass band. Specified in
-% normalized frequency units. Also called Fpass1. }
-Fp1 = 0.03*2/resp_freq;
-% Fp2 — frequency at the edge of the end of the pass band. Specified in
-% normalized frequency units. Also called Fpass2.
-Fp2 = 28*2/resp_freq;
-% Fst1 — frequency at the edge of the start of the first stop band.
-% Specified in normalized frequency units. Also called Fstop1.
-Fst1 = 0.01*2/resp_freq;
-% Fst2 — frequency at the edge of the start of the second stop band.
-% Specified in normalized frequency units. Also called Fstop2.
-Fst2 = 30*2/resp_freq;
-% Ap — amount of ripple allowed in the pass band. Also called Apass.
-Ap = 1;
-% Ast1 — attenuation in the first stop band in decibels (the default
-% units). Also called Astop1.
-Ast1 = 30;
-Ast2 = Ast1;
-Hf = fdesign.bandpass('Fst1,Fp1,Fp2,Fst2,Ast1,Ap,Ast2',Fst1,Fp1,Fp2,Fst2,Ast1,Ap,Ast2);
-% Equiripple designs result in the lowpass
-% filter with the smallest possible order to meet a set of specifications.
-Hd = design(Hf,'equiripple','systemobject',true);
-measure(Hd)
-hfvt = fvtool(Hd,'Color','White');
-legend(hfvt,'Equiripple design')
-
-% -------------------- Cosmetic changes -----------------------------------
-% My Red Color
-myColor = [204 0 0]/255;
-% set(gca,'FontSize', 12)
-legend({'Equiripple design'}, 'FontSize', 14)
-figureHandle = gcf;
-%# make all text in the figure to size 14 and bold
-set(findall(figureHandle,'type','text'),'fontSize',14)
-gcachild = get(gca,'Children');
-set(gca, 'Xcolor', 'k');
-set(gca, 'Ycolor', 'k');
-set(gcachild, 'Linewidth', 2);
-% Change plot marker
-hchildren = get(hfvt,'children');
-haxes = hchildren(strcmpi(get(hchildren,'type'),'axes'));
-hline = get(haxes,'children');
-set(hline(1), 'Color', [0 0 204]/255, 'LineWidth',2); 
-set(hline(2), 'Color',  myColor, 'LineWidth',2);
-% set(hline(3), 'Color', 'k', 'LineWidth',2);
-
-%% Apply filter to the signal
-b =  get(Hd,'Numerator');   % N = 75
-a = 1;  
-respFilt = filtfilt(b, a, physio.Resp);
-
-%% Plot filter and unfiltered signals
-figure;
-plot(t,physio.Resp)
-hold on
-plot(t,respFilt)
-xlim([115 118]);
-
-%% Compute resampling ratio
-% Free up memory
-clear b physio
-pack
-% resampling at p/q times the original sampling rate
-q = round(resp_freq);
-p = round(Fs);
-% Cut data to experiment length
-nT = size(y,2)*resp_freq;
-respFilt = respFilt(1:nT);
-t = t(1:nT);
-
-%% Memory-mapping
-tic
-clear im_obj
-fmem_name=fullfile(pathNameParent, [IOI.subj_name '_resampled.dat']);
-fid = fopen(fmem_name,'w');
-nPix = size(y,1)+1;             % Number of pixel time-traces
-fwrite(fid, zeros([nPix nT]), 'double');
-fclose(fid);
-im_obj = memmapfile(fmem_name,...
-        'Format',{'double' [nPix nT] 'Frames'}, ...
-        'writable',true);
-toc    
-
-%% Interpolate & Resample
-% [ECGresamp,b] = resample(ECGfilt,p,q);
-tic
-ioi_text_waitbar(0, 'Please wait...');
-% Concatenate data
-im_obj.Data.Frames(1,:) = respFilt;
-% [yResamp,b] = resample(y,q,p);
-
-for iPix = 2:nPix
-    im_obj.Data.Frames(iPix,:) = resample(y(iPix-1,:),q,p);
-    % Update progress bar
-      ioi_text_waitbar(iPix/nPix, sprintf('Processing event %d from %d', iPix, nPix));
-end
-ioi_text_waitbar('Clear');
-toc
-
-%% Write data to be imported in Analyzer2
-fwrite_NIR(fullfile(pathNameParent,[IOI.subj_name '_resp_noCB.nir']), im_obj.Data.Frames);
+%% Batch processing of data to be exported to analyzer 2. Do not forget to create the header files
+OK = ioi_export_2analyzer (pathNameParent, pathNamePhysio)
 
 % EOF
