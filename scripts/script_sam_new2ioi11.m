a766547eba48fd6b4fde0e8d8dba249915e6d966
guevaracodina <edgar.guevara@polymtl.ca>
2016-06-21 13:28:37 -0400

    script_sam_new2ioi11.m updated, need to fix nii files.
    Signed-off-by: guevaracodina <edgar.guevara@polymtl.ca>

--- scripts/script_sam_new2ioi11.m
+++ scripts/script_sam_new2ioi11.m
@@ -1,5 +1,6 @@
 %% Path (after running spm8)
 addpath(genpath('D:\spm8\toolbox\ioi'))
+
 %% dir field
 IOI.warning = {};
 IOI.subj_name = 'FB25E02';
@@ -16,9 +17,33 @@ fileID=fopen(fullfile(IOI.dir.dir_subj_raw,'HbO.bin'),'r');
 HbO = fread(fileID,'int32');
 HbO = reshape(HbO,Temps_d1,X_d2,Y_d3); %Temps_d1,… are stored in Dim_binFile.mat
 
+%% Shrinkage preparation and original brainmask
+shrink_factor = 2;
+n_frames = size(HbO,1);
+IOI.sess.n_frames = n_frames;
+nx = round(size(HbO,2)/shrink_factor);
+ny = round(size(HbO,3)/shrink_factor);
+HbO_resize = zeros([size(HbO,1) nx ny]);
+brainMask = mat2gray(squeeze(mean(HbO,1)));
+brainMask = im2bw(brainMask, max(brainMask(:))-eps);
+IOI.res.shrinkageOn = 1;
+% Only if shrinkage is chosen //EGC
+IOI.res.shrink_x = shrink_factor;
+IOI.res.shrink_y = shrink_factor;
+save(IOI.dir.dir_subj_res,'IOI');
+
+%% Actual Shrinkage
+tic
+for iFrames = 1:n_frames,
+    HbO_resize(iFrames,:,:) = ioi_MYimresize(squeeze(HbO(iFrames,:,:)), [nx ny]);
+end
+toc
+HbO = HbO_resize;
+clear HbO_resize
+
 %% Display images
-maxVal = max(HbO(:));
-minVal = min(HbO(:));
+% maxVal = max(HbO(:));
+% minVal = min(HbO(:));
 % figure;
 % for iTime=1:size(HbO,1)
 %     imagesc(squeeze(HbO(iTime,:,:)),[minVal, maxVal]);
@@ -43,7 +68,7 @@ IOI.dev.TR = 0.2;
 IOI.conc.baseline_hbt = 100;
 IOI.conc.baseline_hbo = 60;
 IOI.conc.baseline_hbr = 40;
-
+save(IOI.dir.dir_subj_res,'IOI');
 %% Anatomical
 % Bregma est environ à la ligne 250, colonne 400 et Lambda: 250, 150.
 suffix_for_anat_file = 'anat'; %to build anatomical image name
@@ -87,7 +112,8 @@ if ~isfield(IOI, 'fcIOS')
     IOI.fcIOS(1).SPM = struct([]);
     IOI.fcIOS(1).corr = struct([]);
 end
-brainMask = im2bw(squeeze(mat2gray(mean(HbO,1))),1-eps);
+% brainMask = (ioi_MYimresize(squeeze(mean(HbO,1)), shrink_factor*[nx ny]));
+% brainMask=im2bw(brainMask, max(brainMask(:))-eps);
 imwrite(brainMask, fullfile(IOI.dir.dir_subj_res,[IOI.subj_name '_brainMask.png']),...
     'BitDepth',1)
 [dirName fileName fileExt] = fileparts(IOI.res.file_anat);
@@ -113,8 +139,6 @@ save(IOI.dir.dir_subj_res,'IOI');
 
 %% Save concentrations (HbO)
 IOI.sess_res{1, 1}.missingFrames = [];
-n_frames = size(HbO,1);
-IOI.sess.n_frames = n_frames;
 nzero_padding = 5;
 image_str_start = gen_num_str(1,nzero_padding);
 image_str_last = gen_num_str(IOI.sess.n_frames,nzero_padding);
@@ -123,7 +147,7 @@ str_HbO = 'O';
 % IOI.sess_res{1} = sess;
 % IOI.sess_res{s1}.fname{IOI.color.eng==str_HbO} = fname_new_HbO_list;
 % fname_new_HbO = regexprep(fname,['_OD',tmp_str_avail_color] ,tmp_str_HbO);
-fname_new_HbO = fullfile(IOI.dir.dir_subj_res,...
+fname_new_HbO = fullfile(IOI.dir.dir_subj_res,'S01',...
     [IOI.subj_name '_OD_' str_HbO '_S01_' image_str '.nii']);
 fname_new_HbO_list = {};
 fname_new_HbO_list = [fname_new_HbO_list; fname_new_HbO];
@@ -131,9 +155,11 @@ HbO = permute(HbO,[2 3 4 1]);
 % size(image_hbo) = 303   321     1   128
 % fname_new_HbO =
 % D:\Edgar\OIS_Results\12_10_18,NC09\S01\12_10_18,NC09_O_S01_00001to00128.nii
-
-ioi_save_nifti(image_hbo, fname_new_HbO, vx_anat);
-IOI.sess_res{s1}.fname{IOI.color.eng==str_HbO} = fname_new_HbO_list;
+vx_Hb = [2 2 1];
+tic
+ioi_save_nifti(HbO, fname_new_HbO, vx_Hb);
+toc
+IOI.sess_res{1}.fname{IOI.color.eng==str_HbO} = fname_new_HbO_list;
 IOI.res.concOK = 1;
 % Save IOI matrix
 save(IOI.dir.dir_subj_res,'IOI');
