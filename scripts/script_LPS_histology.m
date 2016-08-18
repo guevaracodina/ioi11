0c76e8e47cc987744a1d9964d6e1e408e413dcdc
guevaracodina <edgar.guevara@polymtl.ca>
2016-08-18 17:08:13 -0500

    script_LPS_histology.m - Good prediction with ANN
    Signed-off-by: guevaracodina <edgar.guevara@polymtl.ca>

--- scripts/script_LPS_histology.m
+++ scripts/script_LPS_histology.m
@@ -99,117 +99,79 @@ group(9:13,:) = {'LPS'};
 
 %% Histology data
 % LP02, LP03, LP04 & LP05
-LPS = [1363882; 2332350; 1121757; 11627024];
+LPShist.L = [2002507; 2149179; 957905; 6718538];
+LPShist.R = [168662; 1142728; 909365; 6718538];
+LPShist.T = LPShist.L + LPShist.R;
 % NC07, NC08 & NC09
-NaCl = [273222; 258777; 272253];
+NaClhist.L = [122725; 135333; 194805];
+NaClhist.R = [110830; 159154; 198182];
+NaClhist.T = NaClhist.L + NaClhist.R;
+% Indices of subjects to keep
+idx2keep = [6:8, 10:13]';
 
 %% Crop X-data
 groupLabels = {'NC01'; 'NC02'; 'NC03'; 'NC05'; 'NC06'; 'NC07'; 'NC08'; 'NC09';...
     'LP01'; 'LP02'; 'LP03'; 'LP04'; 'LP05'};
-idx2keep = [6:8, 10:13]';
-% idx2keep = [1:13]';
+groupLabels = groupLabels(idx2keep);
 xdata = xdata(idx2keep,:);
-ydata = [NaCl; LPS];
+ydata = [NaClhist.T; LPShist.T];
 group = group(idx2keep);
 
-%% Use svr_trainer
-addpath('C:\Edgar\Dropbox\Matlab')
-svrobj = svr_trainer(xdata(1:6,:),ydata(1:6,:),400,0.000000025,'gaussian',0.5);
-y = svrobj.predict(xdata(7,:));
-
 %% k-fold Cross validation using SVM classifier
-% addpath(genpath('D:\Edgar\biolearning'))
 clc
 k = 5;                                     % Number of folds
+cvFolds = crossvalind('Kfold', group, k);   %# get indices of k-fold CV
+% Initialize performance trackers
+pred = [];
+groundTruth = [];
+targetLabels = {};
+for i = 1:k                                 %# for each fold
+    testIdx = (cvFolds == i);               %# get indices of test instances
+    trainIdx = ~testIdx;                    %# get indices training instances
+    
+    xtrain = xdata(trainIdx,:);
+    ytrain = ydata(trainIdx,:);
+    
+    xtest = xdata(testIdx,:);
+    ytest = ydata(testIdx,:);
+    
+    % Bayesian regularization (takes a Q x 56 matrix, where Q = No. samples)
+    pred = [pred; myNeuralNetworkFunction(xtest')'];
 
-% CVO = cvpartition(group,'k',10);
-% boxconstraint — One strategy is to try a geometric sequence of the box
-% constraint parameter. For example, take 11 values, from 1e-5 to 1e5 by a
-% factor of 10.
-% 
-% 
-% rbf_sigma — One strategy is to try a geometric sequence of the RBF sigma
-% parameter. For example, take 11 values, from 1e-5 to 1e5 by a factor of
-% 10.
-
-SigmaVals = logspace(-5, 5, 11);
-BoxVals = logspace(-5, 5, 11);
-ioi_text_waitbar(0, 'Please wait...');
-for iSigma = 1:numel(SigmaVals),
-    for iBox = 1:11,
-        cvFolds = crossvalind('Kfold', group, k);   %# get indices of k-fold CV
-%         cp = classperf(ydata);                      %# init performance tracker
-        pred = [];
-        groundTruth = [];
-        for i = 1:k                                 %# for each fold
-            testIdx = (cvFolds == i);               %# get indices of test instances
-            trainIdx = ~testIdx;                    %# get indices training instances
-            
-            xtrain = xdata(trainIdx,:);
-%             ytrain = group(trainIdx);
-            ytrain = ydata(trainIdx,:);
-            xtest = xdata(testIdx,:);
-%             ytest = group(testIdx);
-            ytest = ydata(testIdx,:);
-            
-            %     minfn = @(z)crossval('mcr',xdata,group,'Predfun', ...
-            %     @(xtrain,ytrain,xtest)crossfun(xtrain,ytrain,...
-            %     xtest,exp(z(1)),exp(z(2))),'partition',CVO);
-            
-            % opts = optimset('TolX',5e-4,'TolFun',5e-4);
-            % [searchmin, fval] = fminsearch(minfn,randn(2,1),opts);
-            
-            % z = exp(searchmin);
-            
-            % rbf_sigma = z(1); %0.8
-            rbf_sigma = SigmaVals(iSigma);
-            % boxconstraint = z(2); % 2e-1
-            boxconstraint = BoxVals(iBox);
-            %# train an SVM model over training instances
-%             svmModel = svmtrain(xtrain, ytrain, ...
-%                 'Autoscale',true, 'Showplot',false, 'Method','SMO', ...
-%                 'BoxConstraint',boxconstraint, 'Kernel_Function','rbf', ...
-%                 'RBF_Sigma',rbf_sigma, 'tolkkt', 1e-6);
-            svmModel = svr_trainer(xtrain,ytrain,boxconstraint,1e-6,'gaussian',rbf_sigma);
-            %# test using test instances, save output labels to create confusion matrix
-%             pred{i} = svmclassify(svmModel, xtest, 'Showplot',false);
-            pred  = [pred; svrobj.predict(xtest)];
-            % Keep target labels to create confusion matrix
-            groundTruth = [groundTruth; ytest];
-            
-            %# evaluate and update performance object
-%             cp = classperf(cp, pred{i}, testIdx);
-            close all
-        end
-        corrIdx(iSigma, iBox) = corr(groundTruth, pred);
-        %# get accuracy
-%         fprintf('Accuracy  = %0.2f %%\n', 100*cp.CorrectRate);
-%         accMat(iSigma, iBox) = cp.CorrectRate;
-        % disp(cp.ClassLabels');
-        %# get confusion matrix
-        %# columns:actual, rows:predicted, last-row: unclassified instances
-        % disp(cp.CountingMatrix);
-%         fprintf('\t\t%s\t%s\n %s\t\t%d\t%d\n %s\t\t%d\t%d\n Unclass\t%d\t%d\n',cp.ClassLabels{1}, cp.ClassLabels{2}, cp.ClassLabels{1},...
-%             cp.CountingMatrix(1,:), cp.ClassLabels{2}, cp.CountingMatrix(2,:), cp.CountingMatrix(3,:));
-    end
-    ioi_text_waitbar(iSigma/numel(SigmaVals), sprintf('Processing event %d from %d', ...
-        iSigma, numel(SigmaVals)));
+    % Keep target values to create correlation plot
+    groundTruth = [groundTruth; ytest];
+    
+    % Keep target labels 
+    targetLabels = [targetLabels; groupLabels(testIdx)];
 end
-ioi_text_waitbar('Clear');
-
-%% 
-pred = -(pred - max(pred));
-pred = pred .* max(groundTruth)/max(pred);
+        
+%% Plot correlation between measured and predicted values
 p = polyfit(groundTruth, pred, 1);
 yfit = p(1)*groundTruth + p(2);
-figure; hold on;
-plot(groundTruth([1 4 6]), pred([1 4 6]), 'ko')
-plot(groundTruth([2 3 5 7]), pred([2 3 5 7]), 'rx')
-plot(groundTruth, yfit, 'b-')
-legend({'NaCl' 'LPS' 'Linear fit'}, 'Location', 'SouthEast')
-% xlim([0 12e6])
-% ylim([0 12e6])
-xlabel('Measured Lesion size (pixels)'); ylabel('Predicted lesion size (pixels)')
 r = corr(groundTruth, pred);
 RMSEP = sqrt(sum((groundTruth-pred).^2)/size(xdata,1));
+
+% Find indices of LPS
+idxLPS = find(~cellfun(@isempty, regexp(targetLabels,'LP')));
+idxNaCl = find(~cellfun(@isempty, regexp(targetLabels,'NC')));
+
+figure; hold on;
+% Plot Predicted = Measured
+plot(groundTruth, groundTruth, 'k:', 'LineWidth', 3)
+% Plot fit
+plot(groundTruth, yfit, 'b-', 'LineWidth', 3)
+% Plot NaCl
+plot(groundTruth(idxNaCl), pred(idxNaCl), 'ko', 'LineWidth', 3, 'MarkerSize', 12)
+% Plot LPS
+plot(groundTruth(idxLPS), pred(idxLPS), 'rx', 'LineWidth', 3, 'MarkerSize', 12)
+
+axis equal
+legend({'Predicted = Measured' 'Linear fit' 'NaCl' 'LPS' }, 'Location', 'SouthEast')
+set(gca, 'FontSize', 14)
+xlim([min(groundTruth) max(groundTruth)])
+ylim([min(groundTruth) max(groundTruth)])
+xlabel('Ventricular Lesion Size (pixels)', 'FontSize', 14); 
+ylabel('Predicted Ventricular Size (pixels)', 'FontSize', 14)
+title(sprintf('r = %0.4f', r), 'FontSize', 14); 
+
 % EOF
