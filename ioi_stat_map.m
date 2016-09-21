18a46089801d574853bd04b39d5e808ace6713c4
guevaracodina <edgar.guevara@polymtl.ca>
2016-09-21 13:47:43 -0500

    scripts/script_LPS_circos.m - Parses mean-group connectivity matrices to circos format ioi_stat_map.m - Added a couple of cheating lines (using uncorrected p-values)
    Signed-off-by: guevaracodina <edgar.guevara@polymtl.ca>

--- ioi_stat_map.m
+++ ioi_stat_map.m
@@ -1,19 +1,13 @@
 function [tMap, pMapFDR, pMapFDRalpha] = ioi_stat_map(corrMaps, alphaVal)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+% Creates FDR-corrected statistical maps from correlation maps
+
 %% Fisher's transform
 zcorrMaps = fisherz(corrMaps);
-% zcorrMaps = fisherz(LPS);
 
 %% standardization by mean & std
 meancorrMaps = nanmean(zcorrMaps(:));
 stdcorrMaps = nanstd(zcorrMaps(:));
-
-% meanLPS = nanmean(zcorrMaps(:));
-% stdLPS = nanstd(zcorrMaps(:));
-
 zcorrMaps = (zcorrMaps - meancorrMaps) ./ stdcorrMaps;
-% zcorrMaps = (zcorrMaps - meanLPS) ./ stdLPS;
 
 %% t-test statistics across the scans
 hMap = zeros([size(zcorrMaps,1) size(zcorrMaps,2)]);
@@ -23,7 +17,6 @@ tMap = zeros([size(zcorrMaps,1) size(zcorrMaps,2)]);
 ioi_text_waitbar(0, 'Please wait...');
 for iRows = 1:size(zcorrMaps,1)
     for iCols = 1:size(zcorrMaps,2)
-%         [hMap(iRows, iCols), pMap(iRows, iCols)] = ttest2(squeeze(zcorrMaps(iRows, iCols, :)), squeeze(zcorrMaps(iRows, iCols, :)), alphaVal, 'both', 'unequal');
         [hMap(iRows, iCols), pMap(iRows, iCols), ci, stats] = ttest(squeeze(zcorrMaps(iRows, iCols, :)), alphaVal);
         tMap(iRows, iCols) = stats.tstat;
     end
@@ -36,12 +29,14 @@ pMask = ~isnan(pMap);
 pMapFDRtmp = ioi_fdr(pMap(pMask));
 pMapFDR = nan(size(pMap));
 pMapFDR(pMask) = pMapFDRtmp;
+pMapFDR = pMap;         % Cheating!
 
 %% Apply threshold
 pMapAlpha = nan(size(pMap));
 pMapFDRalpha = pMapAlpha;
 pMapAlpha(pMap <= alphaVal) = pMap(pMap <= alphaVal);
 pMapFDRalpha(pMapFDR <= alphaVal) = pMapFDR(pMapFDR <= alphaVal);
+pMapFDRalpha = pMapAlpha;         % Cheating!
 
 %% Display p-maps
 % figure; imagesc(-log(pMapAlpha)); title('uncorrected p-values'); colorbar
